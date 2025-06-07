2c4b8762e67e1a47cf38b2e9e2ca2248f3a05973
Zach Collins <34733338+zachc71@users.noreply.github.com>
2025-06-07 03:53:08 -0500

    Clean up main app and game state

--- lib/models/game_state.dart
+++ lib/models/game_state.dart
@@ -40,30 +40,6 @@ class GameState {
     if (atFinalMilestone) {
       prestige.gainPoint();
       resetProgress();
-import 'progression.dart';
-
-class GameState {
-  int mealsServed;
-  int _tierIndex;
-
-  GameState({this.mealsServed = 0}) : _tierIndex = 0;
-
-  ProgressionTier get currentTier => progressionTiers[_tierIndex];
-
-  ProgressionTier? get nextTier =>
-      _tierIndex + 1 < progressionTiers.length
-          ? progressionTiers[_tierIndex + 1]
-          : null;
-
-  void cookMeal() {
-    mealsServed++;
-    _checkForTierUnlock();
-  }
-
-  void _checkForTierUnlock() {
-    final next = nextTier;
-    if (next != null && mealsServed >= next.unlockRequirement) {
-      _tierIndex++;
     }
   }
 }
