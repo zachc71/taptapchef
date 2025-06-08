b6820ecf61205a8e0d75e536c985e9298117455a
Zach Collins <34733338+zachc71@users.noreply.github.com>
2025-06-08 18:10:08 -0500

    Move milestone and panel constants

--- lib/main.dart
+++ lib/main.dart
@@ -19,87 +19,8 @@ import 'widgets/staff_panel.dart';
 import 'widgets/mini_game_dialog.dart';
 import 'widgets/frenzy_overlay.dart';
 import 'widgets/milestone_overlay.dart';
-
-const List<String> milestoneArt = [
-  r'''
-  ( )
- /|_|\\
- /___\\
-  ''',
-  r'''
-  |~~~~|
-  |DNR |
-  |____|
-  ''',
-  r'''
-   _____
-  |[ ] |
-  |[ ] |
-  |____|
-  ''',
-  r'''
-  .----.
- / .--. \\
-| |    | |
- \\ '--' /
-  `----'
-  ''',
-  r'''
-  .-.
- (o o)
-  |=|
- __|__
-/_____\\
-  ''',
-  r'''
-  (X_X)
-   /|\\
-  /_|_\\
-  ''',
-  r'''
-  <\\/>
-  (oo)
-  /||\\
-  ''',
-  r'''
-   *-|-*
-  /o\\
-  \\v/
-  ''',
-];
-
-const List<String> milestoneDialogues = [
-  "Street cart fans now chant your menu. This is totally normal.",
-  "Your diner draws crowds - and a new existential crisis.",
-  "Congrats, you're corporate! Please ignore the accountants in the kitchen.",
-  "Entire nations line up. Is this still about food?",
-  "Starships detour just to taste your fries. Space smells like success.",
-  "You've fed the galaxy. Nothing left but existential hunger.",
-  "Time loops around the kitchen. Omelettes now cause deja vu.",
-  "The multiverse demands endless specials. Hope you're not out of ideas."
-];
-
-const List<String> upgradePanelTitles = [
-  'Cart Upgrades',
-  'Diner Renovations',
-  'Corporate Overhauls',
-  'Global Improvements',
-  'Galactic Remodels',
-  'Endgame Upgrades',
-  'Temporal Adjustments',
-  'Multiverse Investments',
-];
-
-const List<String> hirePanelTitles = [
-  'Street Crew',
-  'Diner Staff',
-  'Corporate Recruits',
-  'Global Recruiting',
-  'Cosmic Crew',
-  'Endgame Operatives',
-  'Temporal Staff',
-  'Multiverse Talent',
-];
+import 'constants/milestones.dart';
+import 'constants/panels.dart';
 
 void main() => runApp(const ProviderScope(child: MyApp()));
 
